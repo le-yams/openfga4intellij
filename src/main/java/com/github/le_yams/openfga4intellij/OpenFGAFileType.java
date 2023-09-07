cceb8b6cad9a7e26705db0afa650bb9beb903209
Yann D'Isanto <yann.disanto@agicap.com>
2023-09-07 11:56:06 +0200

    feat: add fga extension by default

--- src/main/java/com/github/le_yams/openfga4intellij/OpenFGAFileType.java
+++ src/main/java/com/github/le_yams/openfga4intellij/OpenFGAFileType.java
@@ -29,7 +29,7 @@ public class OpenFGAFileType extends LanguageFileType {
     @NotNull
     @Override
     public String getDefaultExtension() {
-        return "openfga";
+        return "fga";
     }
 
     @Nullable
