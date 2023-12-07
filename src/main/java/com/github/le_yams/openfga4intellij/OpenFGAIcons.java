bc3ed6b4c6bc3891fd6237055563cc8c0f5c69ab
Yann D'Isanto <ydisanto@gmail.com>
2023-12-07 16:23:29 +0100

    chore: update tool window icon

--- src/main/java/com/github/le_yams/openfga4intellij/OpenFGAIcons.java
+++ src/main/java/com/github/le_yams/openfga4intellij/OpenFGAIcons.java
@@ -7,6 +7,6 @@ import javax.swing.*;
 public interface OpenFGAIcons {
 
     Icon FILE = IconLoader.getIcon("/icons/openfga-color-transparent-16x16.svg", OpenFGAIcons.class);
-    Icon TOOL_WINDOW = IconLoader.getIcon("/icons/openfga-monochrome-transparent-13x13.svg", OpenFGAIcons.class);
+    Icon TOOL_WINDOW = IconLoader.getIcon("/icons/tool-window.svg", OpenFGAIcons.class);
 
 }
